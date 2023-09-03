c75559fcc842175907c60ac173b2912d2f969fe9
RCSilva1964 <robertocostaesilva@hotmail.com>
2023-09-03 13:12:13 -0300

    Cap. 14 ok!

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -5,7 +5,10 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(AmuletOfYala)]
-pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
+pub fn end_turn(ecs: &SubWorld,
+    #[resource] turn_state: &mut TurnState,
+    #[resource] map: &Map
+) {
     let mut player_hp = <(&Health, &Point)>::query()
         .filter(component::<Player>());
     let mut amulet = <&Point>::query()
@@ -17,11 +20,12 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
         TurnState::MonsterTurn => TurnState::AwaitingInput,
         _ => current_state
     };
-
+    
+    let amulet_default = Point::new(-1, -1);
     let amulet_pos = amulet
         .iter(ecs)
         .next() // .nth(0)
-        .unwrap();
+        .unwrap_or(&amulet_default);
 
     player_hp.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
@@ -30,6 +34,10 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
 
         if pos == amulet_pos {
             new_state = TurnState::Victory;
+        }
+        let idx = map.point2d_to_index(*pos);
+        if map.tiles[idx] == TileType::Exit {
+            new_state = TurnState::NextLevel;
         }
 
     });
